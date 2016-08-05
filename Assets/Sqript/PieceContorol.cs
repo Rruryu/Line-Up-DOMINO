5b4993999ff32ae80cc904427c57bb86447d7b25
kishiya <kishiya100114@gmai.com>
2016-08-05 13:00:33 +0900

    左クリックでの牌の回転

---
+++ Assets/Sqript/PieceContorol.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceContorol : MonoBehaviour {
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    public float y = 0f;
+	// Use this for initialization
+	void Start () 
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    //左クリックしたら
+        if(Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit))
+            {
+                Collider col = hit.collider;
+                var v = new Vector3(0, 90f, 0);
+                col.transform.Rotate(v);
+                Debug.Log("test");
+            }
+        }
+	}
+}
---
+++ Assets/Sqript/PieceKeyMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+    bool click = false;
+	// Use this for initialization
+	void Start () 
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(click)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit = new RaycastHit();
+                if (Physics.Raycast(ray, out hit))
+                {
+                    
+                }
+            }
+        }
+	}
+}
