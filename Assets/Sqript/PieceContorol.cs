9fd0cc46cacad08a1df67f90bf547bf151f1e361
kishiya <kishiya100114@gmail.com>
2016-08-15 02:07:05 +0900

    RayerMaskでのオブジェクトの判別

--- Assets/Sqript/PieceContorol.cs
+++ Assets/Sqript/PieceContorol.cs
@@ -7,43 +7,70 @@ public class PieceContorol : MonoBehaviour {
     private Vector3 offset;
     public float y = 0f;
     string objName;
-    GameObject clickobj;
+    string CubeName;
+    public GameObject Clickobj;
+    public GameObject Ccube;
+    bool click = false;
 	// Use this for initialization
 	void Start () 
     {
-
+        
 	}
 	
 	// Update is called once per frame
-	void Update () 
+    void Update()
     {
-	    //左クリックしたら
-        if(Input.GetMouseButtonUp(0))
+        //LayerMaskの宣言
+        int Piece = LayerMask.GetMask(new string[] { LayerMask.LayerToName(8) });
+        int ClearCube = LayerMask.GetMask(new string[] { LayerMask.LayerToName(9) });
+        //Pieceの選択&回転
+        if (Input.GetMouseButtonUp(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit = new RaycastHit();
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, Piece))
             {
                 /*Collider col = hit.collider;
                 var v = new Vector3(0, 90f, 0);
                 col.transform.Rotate(v);
                 Debug.Log("test");*/
                 objName = hit.collider.gameObject.name;
-                clickobj = GameObject.Find(objName);
+                Clickobj = GameObject.Find(objName);
+                click = true;
             }
         }
         if (Input.GetKeyUp(KeyCode.RightArrow))
         {
             var v = new Vector3(0, 90f, 0);
-            clickobj.transform.Rotate(v);
+            Clickobj.transform.Rotate(v);
         }
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             var v = new Vector3(0, -90f, 0);
-            clickobj.transform.Rotate(v);
+            Clickobj.transform.Rotate(v);
         }
-
-	}
+        //pieceの移動(ClearCube戸の入れ替え)
+        if (Input.GetMouseButtonUp(0) && click)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity, ClearCube))
+            {
+                CubeName = hit.collider.gameObject.name;
+                Ccube = GameObject.Find(CubeName);
+                Debug.Log(CubeName);
+            }/*
+            if()
+            {
+                
+            }
+            else
+            if()
+            {
+            
+            }*/
+        }
+    }
     /*
     void OnMouseDown()
     {
